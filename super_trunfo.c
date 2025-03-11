c2cafc752b8c9f005fd90964b447b4a4473ef37e
Jheot <josueinfoctt@gmail.com>
2025-03-11 02:12:28 +0000

    Comecei

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,6 +1,13 @@
 #include <studio.h>
 
 int main (){
-
+    char estado;
+    int população;
+    float 
     
+    printf("Desafio Super Trunfo! \n");
+
+    printf ("Carta 1");
+    printf ("Nome da Cidade");
+    scanf("") // krl vou terminar depois essa porra   
 }
